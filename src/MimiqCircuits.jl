53f0fd5e982ae72bd3d939f736bb7605ab5ec9f1
Guido Masella <guido.masella@qperfect.io>
2023-11-14 01:44:40 +0100

    version: v0.6.1

--- src/MimiqCircuits.jl
+++ src/MimiqCircuits.jl
@@ -19,7 +19,7 @@ module MimiqCircuits
 using Reexport
 using SHA
 using JSON
-using QCSR
+using Statistics
 using MimiqLink
 using RecipesBase
 using PrettyTables
@@ -39,8 +39,8 @@ import Measures
     Connection,
     Execution
 
-export Results
 export execute
+export executeqasm
 export printreport
 export getinputs
 export getresults
@@ -58,40 +58,28 @@ const MAX_BONDDIM = 2^12
 # default bond dimension
 const DEFAULT_BONDDIM = 256
 
-# maximum time limit allowed
-const MAX_TIME_LIMIT = 30 * 60
-
 # default time limit
 const DEFAULT_TIME_LIMIT = 5 * 60
 
-struct Results
-    ex::Execution
-    results::Dict
-    samples::Dict{BitState, Int64}
-    amplitudes::Dict{BitState, ComplexF64}
-end
+const RESULTSPB_FILE = "results.pb"
+const CIRCUITPB_FILE = "circuit.pb"
+const CIRCUITQASM_FILE = "circuit.qasm"
 
 function _ptime(time::Number)
     if time < 1e-6
         return @sprintf "%.3gns" time * 1e9
-    end
-    if time < 1e-3
+    elseif time < 1e-3
         return @sprintf "%.3gµs" time * 1e6
-    end
-    if time < 1e0
+    elseif time < 1e0
         return @sprintf "%.3gms" time * 1e3
+    elseif time < 60
+        return @sprintf "%.3gs" time
+    else
+        hours = floor(time / 3600)
+        minutes = floor((time - hours * 3600) / 60)
+        seconds = time - hours * 3600 - minutes * 60
+        return @sprintf "%02ih %02im %.3gs" hours minutes seconds
     end
-    return @sprintf "%.3gs" time
-end
-
-function Base.show(io::IO, r::Results)
-    tot_time = sum(values(r.results["time"]))
-    println(io, "Results of execution $(r.ex.id):")
-    println("├── algorithm: ", r.results["algorithm"])
-    println("├── time: ", _ptime(tot_time))
-    println("├── fidelity: ", r.results["fidelity"])
-    println("├── sampled $(length(r.samples)) bitstrings")
-    print("└── returned $(length(r.amplitudes)) amplitudes")
 end
 
 function _to_iobuffer(s::AbstractString)
@@ -146,16 +134,13 @@ function execute(
     tempdir = mktempdir(; prefix="mimiq_")
 
     # write the circuit to a file
-    circuitfile = joinpath(tempdir, "circuit.json")
-
-    open(circuitfile, "w") do io
-        write(io, MimiqCircuitsBase.tojson(c))
-    end
+    circuitfile = joinpath(tempdir, CIRCUITPB_FILE)
+    saveproto(circuitfile, c)
 
     circuitsha = _gethash(circuitfile)
 
-
     nq = numqubits(c)
+
     if any(b -> numqubits(b) != nq, bitstates)
         throw(
             ArgumentError(
@@ -164,7 +149,7 @@ function execute(
         )
     end
 
-    # prepare the parameters 
+    # prepare the parameters
 
     pars = Dict(
         "algorithm" => algorithm,
@@ -185,14 +170,125 @@ function execute(
     end
 
     # prepare the request
-    if timelimit > MAX_TIME_LIMIT
-        throw(ArgumentError("Time limit should be less than 30 minutes"))
-    end
-
     req = Dict(
         "executor" => "Circuits",
         "timelimit" => timelimit,
-        "files" => [Dict("name" => "circuit.json", "hash" => circuitsha)],
+        "files" => [Dict("name" => CIRCUITPB_FILE, "hash" => circuitsha)],
+        "parameters" => pars,
+        "apilang" => "julia",
+        "apiversion" => pkgversion(@__MODULE__),
+        "circuitsapiversion" => pkgversion(MimiqCircuitsBase),
+    )
+
+    # write the request to a file
+    reqfile = joinpath(tempdir, "parameters.json")
+
+    open(reqfile, "w") do io
+        write(io, JSON.json(req))
+    end
+
+    type = "CIRC"
+
+    return MimiqLink.request(conn, type, algorithm, label, timelimit, reqfile, circuitfile)
+end
+
+# checks if a file is a valid qasm 2 file
+function _check_file_qasm2(f::AbstractString)
+    open(f, "r") do io
+        while true
+
+            line = readline(io)
+
+            if startswith(line, "//")
+                continue
+            end
+
+            if !startswith(line, "OPENQASM 2.0;")
+                throw(ArgumentError("File is not a valid QASM 2.0 file"))
+            end
+
+            # if startswith(line, "OPENQASM 2.0;")
+            break
+        end
+    end
+end
+
+"""
+    executeqasm(connection, qasmfilepath[; kwargs...])
+
+Execute a quantum circuit on the MIMIQ remote services.
+
+The circuit is applied to the zero state and the resulting state is measured via sampling.
+Optionally amplitudes corresponding to few selected bit states (or bitstrings) can be returned from the computation.
+
+## Keyword Arguments
+
+* `label::String`: mnemonic name to give to the simulation, will be visible on the [web interface](https://mimiq.qperfect.io)
+* `algorithm`: algorithm to use by the compuation. By default `"auto"` will select the fastest algorithm between `"statevector"` or `"mps"`.
+* `nsamples::Integer`: number of times to sample the circuit (default: 1000, maximum: 2^16)
+* `bitstates::Vector{BitState}`: list of bit states to compute the amplitudes for (default: `BitState[]`)
+* `timelimit`: number of seconds before the computation is stopped (default: 300 seconds or 5 minutes)
+* `bonddim::Int64`: bond dimension for the MPS algorithm (default: 256, maximum: 4096)
+* `seed::Int64`: a seed for running the simulation (default: random seed)
+"""
+function executeqasm(
+    conn::Connection,
+    qasmfilepath::AbstractString;
+    label::AbstractString="circuitsimu",
+    algorithm::String="auto",
+    nsamples=DEFAULT_SAMPLES,
+    bitstates::Vector{BitState}=BitState[],
+    timelimit=DEFAULT_TIME_LIMIT,
+    bonddim::Union{Nothing, Integer}=nothing,
+    seed::Int64=rand(0:typemax(Int64)),
+)
+    if nsamples > MAX_SAMPLES
+        throw(ArgumentError("Number of samples should be less than 2^16"))
+    end
+
+    tempdir = mktempdir(; prefix="mimiq_")
+
+    # check if the file exists
+    if !isfile(qasmfilepath)
+        throw(ArgumentError("File does not exist"))
+    end
+
+    # checks if the file is a valid qasm 2 file
+    _check_file_qasm2(qasmfilepath)
+
+    # copy the file away
+    circuitfile = joinpath(tempdir, CIRCUITQASM_FILE)
+    cp(qasmfilepath, circuitfile)
+    circuitsha = _gethash(circuitfile)
+
+    if any(b -> numqubits(b) != numqubits(first(bitstates)), bitstates)
+        throw(ArgumentError("Inconsistent bitstates length."))
+    end
+
+    # prepare the parameters
+    pars = Dict(
+        "algorithm" => algorithm,
+        "bitstates" => bitstates,
+        "samples" => nsamples,
+        "seed" => seed,
+    )
+
+    if algorithm == "auto" || algorithm == "mps"
+        if isnothing(bonddim)
+            pars["bondDimension"] = DEFAULT_BONDDIM
+        else
+            if bonddim < MIN_BONDDIM || bonddim > MAX_BONDDIM
+                throw(ArgumentError(("Bond dimension should be ∈[1,4096]")))
+            end
+            pars["bondDimension"] = bonddim
+        end
+    end
+
+    # prepare the request
+    req = Dict(
+        "executor" => "Circuits",
+        "timelimit" => timelimit,
+        "files" => [Dict("name" => CIRCUITQASM_FILE, "hash" => circuitsha)],
         "parameters" => pars,
     )
 
@@ -203,8 +299,9 @@ function execute(
         write(io, JSON.json(req))
     end
 
-    type = "example-type-1"
-    MimiqLink.request(conn, type, algorithm, label, timelimit, reqfile, circuitfile)
+    type = "CIRC"
+
+    return MimiqLink.request(conn, type, algorithm, label, timelimit, reqfile, circuitfile)
 end
 
 """
@@ -216,12 +313,18 @@ function getinputs(conn::Connection, ex::Execution)
     tmpdir = mktempdir(; prefix="mimiq_in_")
     names = MimiqLink.downloadjobfiles(conn, ex, tmpdir)
 
-    if ["parameters.json", "circuit.json"] ⊈ basename.(names)
+    if "parameters.json" ∉ basename.(names)
         error("$ex is not a valid execution for MimiqCircuits: missing files")
     end
 
     parameters = JSON.parsefile(joinpath(tmpdir, "parameters.json"))
-    circuit = MimiqCircuitsBase.fromjson(JSON.parsefile(joinpath(tmpdir, "circuit.json")))
+
+    if "cirucit.pb" in basename.(names)
+        circuit = loadproto(joinpath(tmpdir, CIRCUITPB_FILE), Circuit)
+    elseif CIRCUITQASM_FILE in basename.(names)
+        circuit = joinpath(tmpdir, CIRCUITPB_FILE)
+        @info "Downloaded QASM input as $circuit"
+    end
 
     return circuit, parameters
 end
@@ -244,83 +347,36 @@ function getresults(conn::Connection, ex::Execution; interval=10)
     infos = MimiqLink.requestinfo(conn, ex)
 
     if infos["status"] == "ERROR"
-        error("Remote job errored.")
+        msg = get(infos, "message", nothing)
+        if isnothing(msg)
+            error("Remote job errored.")
+        else
+            error("Remote job errored: $msg.")
+        end
     end
 
     tmpdir = mktempdir(; prefix="mimiq_res_")
 
     names = MimiqLink.downloadresults(conn, ex, tmpdir)
 
-    if ["results.json", "samples.qcsr", "amplitudes.qcsr"] ⊈ basename.(names)
+    if RESULTSPB_FILE ∉ basename.(names)
         error("$ex is not a valid execution for MimiqCircuits: missing files")
     end
 
-    results = JSON.parsefile(joinpath(tmpdir, "results.json"))
+    res = loadproto(joinpath(tmpdir, RESULTSPB_FILE), QCSResults)
 
-    samples = Dict(
-        map(
-            b -> BitState(first(b)) => last(b),
-            QCSR.load(joinpath(tmpdir, "samples.qcsr")),
-        ),
-    )
-
-    amplitudes = Dict(
-        map(
-            b -> BitState(first(b)) => last(b),
-            QCSR.load(joinpath(tmpdir, "amplitudes.qcsr")),
-        ),
-    )
-
-    Results(ex, results, samples, amplitudes)
+    return res
 end
-
-@recipe function f(res::Results; endianess=:big, max_outcomes=15)
-    x = []
-    y = []
-    for (bs, s) in res.samples
-        push!(x, to01(bs; endianess=endianess))
-        push!(y, s)
-    end
-    ps = sortperm(y; rev=true)
-    permute!(x, ps)
-    permute!(y, ps)
-
-    # NOTE: this should come before the truncation
-    nsamples = sum(y)
-
-    x = x[1:min(length(x), max_outcomes)]
-    nbars = length(x)
-
-    y = y[1:nbars]
-
-    nq = length(first(x))
-
-    size := (800, 400 + 10 * nq)
-    margin := 10Measures.mm
-    bottom_margin := nq * 1.7Measures.mm
-
-    @series begin
-        seriestype := :bar
-        yguide := "Counts / $nsamples"
-        xguide := "Bit State"
-        legend := nothing
-        xrotation := 90
-        xticks := ((1:nbars) .- 0.5, x)
-        fill := "#0c7e8f"
-        return x, y
-    end
-end
-
 
 """
-    printreport(res::MimiqCircuits.Results; kwargs...)
+    printreport(res::Results; kwargs...)
 
 Print a report on the MIMIQ simulation results `res`
 
 ## Keyword Arguments
 * `max_outcomes`: the maximum number of unique measurement outcomes to display (default 8)
 """
-function printreport(res::MimiqCircuits.Results; max_outcomes::Int=8)
+function printreport(res::QCSResults; max_outcomes::Int=8)
     # pretty_table format
     tf = TextFormat(
         up_right_corner='=',
@@ -340,30 +396,44 @@ function printreport(res::MimiqCircuits.Results; max_outcomes::Int=8)
     @printf "===========================\n"
     @printf "Simulation report\n"
     @printf "===========================\n"
-    @printf "Algorithm:         %s\n" res.results["algorithm"]
-    @printf "Execution time:    %s\n" _ptime(res.results["time"]["apply"])
-    @printf "Sampling time:     %s\n" _ptime(res.results["time"]["sampling"])
-    @printf "Fidelity estmate:  %.3f (avg. gate error %.4f)\n" res.results["fidelity"] res.results["averageGateError"]
+    if !isnothing(res.simulator) && !isnothing(res.version)
+        @printf "Simulator: %s %s\n" res.simulator ress.version
+    end
 
-    if length(res.samples) > 0
+    for (k, v) in res.timings
+        @printf "%s time: %s\n" k _ptime(v)
+    end
+
+    if !isempty(res.fidelities)
+        fidelity = extrema(res.fidelities)
+        @printf "Fidelity estimate (min, max): %.3f %.3f" fidelity[1] fidelity[2]
+    end
+
+    if !isempty(res.avggateerrors)
+        avggateerrors = extrema(res.avggateerrors)
+        @printf "Average ≥2-qubit gate error (min, max) %.4f %.4f\n" avggateerrors[1] avggateerrors[2]
+    end
+
+    if !isempty(res.samples)
+        samples = MimiqCircuitsBase.sampleshistrogram(res)
         outcomes =
-            sort(collect(res.samples), by=x -> x.second, rev=true)[1:min(end, max_outcomes)]
+            sort(collect(samples), by=x -> x.second, rev=true)[1:min(end, max_outcomes)]
 
         @printf "\n"
-        @printf "Measurement results\n"
+        @printf "Measurement results (classical registers)\n"
 
-        table = permutedims(hcat([[to01(k), v] for (k, v) in outcomes]...))
+        table = permutedims(hcat([[join(Int.(k), ""), v] for (k, v) in outcomes]...))
         pretty_table(table, header=["state", "samples"], tf=tf)
         if length(outcomes) >= max_outcomes
-            @printf "results limited to %i items, see `res.samples` for a full list\n" max_outcomes
+            @printf "results limited to %i items, see `res.cstates` for a full list\n" max_outcomes
         end
     end
 
-    if length(res.amplitudes) > 0
+    if !isempty(res.amplitudes)
         @printf "\n"
         @printf "Statevector amplitudes\n"
 
-        table = permutedims(hcat([[to01(k), v] for (k, v) in res.amplitudes]...))
+        table = permutedims(hcat([[string(k), v] for (k, v) in res.simres.amplitudes]...))
         pretty_table(table, header=["state", "amplitude"], tf=tf)
     end
 end
